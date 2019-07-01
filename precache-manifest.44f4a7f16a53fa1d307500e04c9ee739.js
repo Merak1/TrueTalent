10b36b4d4bee4b590fcc306cd435fab088c8f747
Merak1 <eddie_comes_for_you@hotmail.es>
2019-07-01 00:16:49 -0500

    Updates

---
+++ precache-manifest.44f4a7f16a53fa1d307500e04c9ee739.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9642b1c89ed851ff85b32b80fccc5ce7",
+    "url": "/trueTalent/index.html"
+  },
+  {
+    "revision": "0a62223be79e8a8d10fc",
+    "url": "/trueTalent/static/css/main.cbe6df7d.chunk.css"
+  },
+  {
+    "revision": "ec17dd9d751765d291bb",
+    "url": "/trueTalent/static/js/2.609907aa.chunk.js"
+  },
+  {
+    "revision": "0a62223be79e8a8d10fc",
+    "url": "/trueTalent/static/js/main.f312aafc.chunk.js"
+  },
+  {
+    "revision": "cb04a77170040e4d5562",
+    "url": "/trueTalent/static/js/runtime~main.1021bc42.js"
+  },
+  {
+    "revision": "c1867229ff58d199334e629eeeadf856",
+    "url": "/trueTalent/static/media/back4.c1867229.png"
+  },
+  {
+    "revision": "20461b6cd55d6b1f178083ff9db18238",
+    "url": "/trueTalent/static/media/ball1.20461b6c.png"
+  },
+  {
+    "revision": "7bab50405ff495c23b855939f3de008b",
+    "url": "/trueTalent/static/media/ball2.7bab5040.png"
+  },
+  {
+    "revision": "27c3d0b969d7b7ac019ac3e4822cf38d",
+    "url": "/trueTalent/static/media/ball3.27c3d0b9.png"
+  },
+  {
+    "revision": "392b54626832bd3a303758c2a5aee542",
+    "url": "/trueTalent/static/media/ball4.392b5462.png"
+  },
+  {
+    "revision": "535e8694dd1af48aeca02bbf7fdbb9f1",
+    "url": "/trueTalent/static/media/bola.535e8694.svg"
+  },
+  {
+    "revision": "49218a7dee739685a2ee76dbb8141244",
+    "url": "/trueTalent/static/media/facebook.49218a7d.svg"
+  },
+  {
+    "revision": "dade1fd8e136dcb28e819d3d1e35220a",
+    "url": "/trueTalent/static/media/header.dade1fd8.png"
+  },
+  {
+    "revision": "c9b37435594b35633f965c990326393b",
+    "url": "/trueTalent/static/media/instagram.c9b37435.svg"
+  },
+  {
+    "revision": "1ff8881de155b45a08ad3334867fbbc5",
+    "url": "/trueTalent/static/media/linkedin.1ff8881d.svg"
+  },
+  {
+    "revision": "7ae437141b6df3b3445f2709f6da12a8",
+    "url": "/trueTalent/static/media/raqueta.7ae43714.png"
+  },
+  {
+    "revision": "216f5101cbfc83623574b720bfbb42ab",
+    "url": "/trueTalent/static/media/twitter.216f5101.svg"
+  },
+  {
+    "revision": "fcca613835f7251b22d3813a9b93f0a0",
+    "url": "/trueTalent/static/media/vimeo.fcca6138.svg"
+  }
+]);
